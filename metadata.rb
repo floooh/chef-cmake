cdc504d89a281b06281cc6e4496a02965d711549
Andre Weissflog <floooh@gmail.com>
2014-01-17 20:17:44 +0100

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'floooh@gmail.com'
 license          'The MIT License (MIT)'
 description      'Install cmake from source tarball'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'build-essential'
 depends          'ark'
